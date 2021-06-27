2d7f2cf9137b16208c8e472fc1892fe578206dd9
doriancodes <dorian.alo.davi@gmail.com>
2021-06-27 19:49:00 +0200

    edited app.json

--- src/main/kotlin/dorian/codes/bottino/PingController.kt
+++ src/main/kotlin/dorian/codes/bottino/PingController.kt
@@ -2,18 +2,20 @@ package dorian.codes.bottino
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class PingController {
 
-    @GetMapping("/keep-alive")
+    @RequestMapping("/keep-alive")
     fun keepAlive(): ResponseEntity<String> {
         return ResponseEntity("ALIVE AND WELL!", HttpStatus.OK)
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     fun home(): ResponseEntity<String> {
         return ResponseEntity("Homepage", HttpStatus.OK)
     }
